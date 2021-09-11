426704d49afdbd7ff38ed7e015c607dd2bc0c618
ShahabAthar25 <shahabathar25@gmail.com>
2021-09-11 21:05:11 +0500

    Add Sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
 import Header from './Component/Header/Header'
+import Sidebar from './Component/Sidebar/Sidebar'
+import Videos from './Component/Videos/Videos';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="app_body">
+        <Sidebar />
+        <Videos />
+      </div>
     </div>
   );
 }
