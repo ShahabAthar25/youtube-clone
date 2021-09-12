038344cec895d8ea9c3e3257c505a9295edbcd89
ShahabAthar25 <shahabathar25@gmail.com>
2021-09-12 15:01:52 +0500

    Add Videos

--- src/Component/Videos/Videos.js
+++ src/Component/Videos/Videos.js
@@ -45,6 +45,8 @@ function Videos() {
                 <VideoCard />
                 <VideoCard />
                 <VideoCard />
+                <VideoCard />
+                <VideoCard />
             </div>
         </div>
     )
